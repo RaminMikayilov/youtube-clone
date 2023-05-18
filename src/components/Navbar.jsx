a59ffe498d2515c8050f8b5ed62d1245cabdedb5
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-18 19:00:35 +0400

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 // icons
 import { AiFillYoutube, AiOutlineSearch } from "react-icons/ai";
 import { ImYoutube2 } from "react-icons/im";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-3 md:px-8 py-3">
-      {/* icons */}
-      <div className="flex text-3xl cursor-pointer">
-        <AiFillYoutube color="#f00" />
-        <ImYoutube2 color="#fff" />
-      </div>
+    <nav className="flex items-center justify-between px-3 md:px-8 py-3 md:py-6 sticky top-0 left-0 bg-cBlack shadow-md">
+      {/* logo */}
+      <Link to="/">
+        <div className="flex text-3xl cursor-pointer">
+          <AiFillYoutube color="#f00" />
+          <ImYoutube2 color="#fff" />
+        </div>
+      </Link>
+
+      {/* searchbar */}
       <form className="flex items-center bg-cGray rounded-full w-3/5 sm:w-1/2 md:w-1/3 md:mx-auto">
         <input
           type="text"
