5d28923683dc97643d272bb70a50bed0e68ac15b
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-18 18:08:16 +0400

    Configurate react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+// components
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+
+// context provider
+import { YoutubeContextProvider } from "./contexts/YoutubeContext";
+
+// react router dom
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return <div>App</div>;
+  return (
+    <YoutubeContextProvider>
+      <div className="bg-cBlack">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Body />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </YoutubeContextProvider>
+  );
 }
 
 export default App;
