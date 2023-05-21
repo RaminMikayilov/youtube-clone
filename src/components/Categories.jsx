638b877a64ac5e3b07721327b097a3d6280b47a2
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-21 21:14:12 +0400

    Update Categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -8,13 +8,14 @@ const Categories = () => {
   const { activeCategory, setActiveCategory } = useYoutubeContext();
 
   return (
-    <div className="flex flex-row md:flex-col w-full md:w-1/5 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide py-2 md:py-0 text-sm md:text-lg">
+    <div className="flex flex-row md:flex-col w-full md:w-1/5 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide py-2 md:py-0 text-sm md:text-lg md:overflow-y-scroll md:h-[85vh] md:border-r-2 border-cGray">
       {categories.map((category, index) => (
         <div
           key={index}
-          className={`text-white px-3 py-2 md:px-4 md:py-3 mr-2 cursor-pointer bg-cGray md:bg-transparent md:hover:bg-cGray duration-300 rounded-lg font-bold flex items-center gap-2 md:gap-4 ${
-            category.name === activeCategory &&
-            "bg-white text-cGray md:text-white md:!bg-cGray"
+          className={`px-3 py-2 md:px-4 md:py-3 mr-2 cursor-pointer bg-cGray md:hover:bg-cGray duration-300 rounded-lg font-bold flex items-center gap-2 md:gap-4 ${
+            category.name === activeCategory
+              ? "bg-white text-cGray md:text-white md:bg-cGray"
+              : "text-white md:bg-transparent"
           }`}
           onClick={() => setActiveCategory(category.name)}
         >
